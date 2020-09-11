8905ca6b16843522d16ae56807252dbf773c7e07
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2020-09-11 22:45:12 +0100

    feat: Add operation and calculation logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,1 +1,79 @@
-import Big from 'big-js';
+import operate from './operate';
+
+const calculate = (dataObj, buttonName) => {
+  let { total, next, operation } = dataObj;
+
+  const ArithmeticOperators = ['+', '-', 'X', '÷'];
+
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+  const modifierOrPercent = ['+/-', '%'];
+  const modifierOrPercentFunc = (number, operator) => {
+    if (next) {
+      next = operate(next, number, operator);
+    } else if (total) {
+      total = operate(total, number, operator);
+    }
+  };
+
+  if (buttonName === 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+  } else if (modifierOrPercent.includes(buttonName)) {
+    if (buttonName === '+/-') {
+      modifierOrPercentFunc(-1, 'X');
+    } else if (buttonName === '%') {
+      modifierOrPercentFunc(100, '÷');
+    }
+  } else if (buttonName === '=') {
+    if (total && next && operation) {
+      total = operate(total, next, operation);
+      next = null;
+      operation = null;
+    }
+  } else if (ArithmeticOperators.includes(buttonName)) {
+    if (total && next && operation) {
+      total = operate(total, next, operation);
+      next = null;
+      operation = buttonName;
+    } else {
+      operation = buttonName;
+    }
+  } else if (numbers.includes(buttonName)) {
+    if (buttonName === '.') {
+      if (next) {
+        next += buttonName;
+      } else if (total) {
+        total += buttonName;
+      } else {
+        next = '0.';
+      }
+    } else if (buttonName !== '.') {
+      if (next) {
+        if (operation) {
+          if (total) {
+            next += buttonName;
+          } else {
+            total = next;
+            next = buttonName;
+          }
+        } else {
+          next += buttonName;
+        }
+      } else if (!next) {
+        if (total) {
+          if (operation) {
+            next = buttonName;
+          } else {
+            total += buttonName;
+          }
+        } else {
+          next = buttonName;
+        }
+      }
+    }
+  }
+  return { total, next, operation };
+};
+
+export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,31 @@
 import Big from 'big-js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  const bigNumberOne = Big(numberOne);
+  let result;
+
+  switch (operation) {
+    case '+':
+      result = bigNumberOne.plus(numberTwo);
+      break;
+    case '-':
+      result = bigNumberOne.minus(numberTwo);
+      break;
+    case 'X':
+      result = bigNumberOne.times(numberTwo);
+      break;
+    case '÷':
+      if (numberTwo === '0') {
+        result = 0;
+      } else {
+        result = bigNumberOne.div(numberTwo);
+      }
+      break;
+    default:
+      result = '';
+      break;
+  }
+  return result.toString();
+};
+
+export default operate;
